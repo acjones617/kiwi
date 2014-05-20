01fa415b235e397ec783c1709de3b38226ce5185
Sean Dokko <dokko1230@gmail.com>
2014-05-19 20:24:22 -0700

    adds firebase config and sendmessage functionality

---
+++ background.js
@@ -0,0 +1,19 @@
+function initBackground() {
+
+  var db = new Firebase('https://kiwidb.firebaseio.com/');
+
+  chrome.browserAction.onClicked.addListener(
+    function(tab) {
+      // console.log('whuttt');
+      // alert('sending message');
+      // chrome.tabs.sendMessage(tab.id, );
+      chrome.tabs.sendMessage(tab.id, { shibal: true }, function(response) {
+        debugger;
+        db.push(response);
+        console.log(response);
+      });
+    }
+  ); 
+}
+
+initBackground();
