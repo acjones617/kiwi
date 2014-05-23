b75fd5a074ed2e0c0af8dc1cc628b3d8dfb82049
Sean Dokko <dokko1230@gmail.com>
2014-05-22 21:15:50 -0700

    adds tests and fixes class concat

---
+++ tests/getPath.js
@@ -0,0 +1,17 @@
+var jQuery = require('jQuery');
+var expect = require('chai').expect;
+
+describe('getPath', function () {
+  it('should get basic path without any ids', function () {
+    
+  });
+  it('should get paths with ids', function () {
+    
+  });
+  it('should get paths with classes', function () {
+    
+  });
+  it('should get path with classes and ids', function () {
+    
+  });
+});
