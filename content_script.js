d76648749fc6d2af4bbe30d9119d9c184d2be09b
Rohan <sarith21@gmail.com>
2014-05-22 18:57:03 -0700

    adds element and class scraping to getPath function. Begins debugging click handler

--- content_script.js
+++ content_script.js
@@ -1,24 +1,38 @@
 jQuery.fn.getPath = function () {
     if (this.length != 1) throw 'Requires one element.';
-
     var path, node = this;
-    while (node.length) {
-        var realNode = node[0], name = realNode.localName;
-        if (!name) break;
-        name = name.toLowerCase();
 
+    if(node[0].getAttribute('id')){ //return id only if it exists
+      var id = node[0].getAttribute('id');
+      return '#' + id;
+    }
+
+    var addClass = true; //to make sure class selector fires once at maximum
+
+    while (node) {
+        var realNode = node[0];
+        if (!realNode.localName) break; //something's wrong with the selected element
+
+        var name = realNode.localName.toLowerCase();
         var parent = node.parent();
-
         var siblings = parent.children(name);
+
         if (siblings.length > 1) {
-            name += ':eq(' + siblings.index(realNode) + ')';
+          if(realNode.getAttribute('class') && addClass){
+            name+= '.' + realNode.getAttribute('class');
+            console.log('added class: ', name);
+            addClass = false;
+          }
+          name += ':eq(' + siblings.index(realNode) + ')';
         }
         if(name !== 'tbody') {
           path = name + (path ? '>' + path : '');
+          console.log('path: ', path);
         }
         node = parent;
     }
 
+    console.log(path);
     return path;
 };
 
@@ -59,6 +73,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   toggleSelectMode();
   if (request.shibal) {
     $('body *').on('click', function(e) {
+      console.log('clicked');
       var selectedText = $(this).text();
       var $el = $(this);
       if(selectedText !== '') {
@@ -70,6 +85,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             text: selectedText,
             url: window.location.href
           };
+          debugger;
           sendResponse(response);
           // $('body *').off('click', '*');
           noticeUser();
