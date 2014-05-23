dc4612c676b8f4c7d9f66600e634cc352880c690
Sean Dokko <dokko1230@gmail.com>
2014-05-22 23:11:27 -0700

    adds crappy tests

--- spec/jQuery.getPath.js
+++ spec/jQuery.getPath.js
@@ -28,11 +28,11 @@ describe('jQuery()', function () {
     $('body').find(dom).remove();
   });
 
-  xit('does pathing with classes', function () {
+  it('does pathing with classes', function () {
     var dom = $('<div class="not"><div class="fun bun hun">Hello</div></div>');
     var target = $('<div></div>');
-    dom.append(target);
     $('body').append(dom);
+    $('.fun').append(target);
 
     expect(target.getPath()).to.equal('html>body>div.not:eq(1)>div.fun.bun.hun:eq(1)');
     expect($(target.getPath())).to.not.equal([]);
@@ -42,8 +42,8 @@ describe('jQuery()', function () {
   xit('does pathing with ids', function() {
     var dom = $('<div id="fun"><div>Hello</div></div>');
     var target = $('<div></div>');
-    dom.append(target);
     $('body').append(dom);
+    $('#fun div').append(target);
 
     expect(target.getPath()).to.equal('html>body>div#fun>div:eq(1)');
     expect($(target.getPath())).to.not.equal([]);
@@ -54,10 +54,22 @@ describe('jQuery()', function () {
   xit('returns single path if target has id', function() {
     var dom = $('<div><div id="fun">Hello</div></div>');
     var target = $('<div></div>');
-    dom.append(target);
     $('body').append(dom);
+    $('#fun').append(target);
 
     expect(target.getPath()).to.equal('#fun');
+    expect($(target.getPath())).to.not.equal([]);
+    $('body').find(dom).remove();
+
+  });
+
+  xit('does pathing with ids', function() {
+    var dom = $('<div id="fun" class="jin"><div class="fun bun hun">Hello</div></div>');
+    var target = $('<div></div>');
+    $('body').append(dom);
+    $('.fun').append(target);
+
+    expect(target.getPath()).to.equal('html>body>div#fun.jin>div.fun.bun.hun:eq(1)');
     expect($(target.getPath())).to.not.equal([]);
     $('body').find(dom).remove();
 
