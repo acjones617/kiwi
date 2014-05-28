e790ae2c850d61c2eef9f227b67591dfc61047b9
Ramin Taleghani <talegram@gmail.com>
2014-05-28 13:40:37 -0700

    tests chrome extension authentication

--- background.js
+++ background.js
@@ -1,17 +1,87 @@
 function initBackground() {
 
-  var db = new Firebase('https://kiwidb.firebaseio.com/');
+  var db = new Firebase('https://kiwidb.firebaseio.com/users/facebook:10103897713367983');
 
   chrome.browserAction.onClicked.addListener(
     function(tab) {
       // console.log('whuttt');
       // alert('sending message');
       // chrome.tabs.sendMessage(tab.id, );
-      chrome.tabs.sendMessage(tab.id, { createKiwi: true }, function(response) {
-        console.log('Right before sending to DB: ', response);
-        console.log('Sending to DB:');
-        db.push(response);
+      
+      // var w = 440;
+      // var h = 220;
+      // var left = (screen.width/2)-(w/2);
+      // var top = (screen.height/2)-(h/2); 
+
+      // chrome.windows.create({
+      //   'url': 'http://localhost:9000/special/login/route', 
+      //   'type': 'popup', 
+      //   'width': w, 
+      //   'height': h, 
+      //   'left': left, 
+      //   'top': top} , function(window) {
+        
+      // });
+      
+/*FOR TESTING OF ADDING NEW USER TO DB*/
+      // var response = {
+      //     'age': 50,
+      //     'kiwis': {
+      //       'kiwiHash01': {
+      //         'url': 'http://www.google.com',
+      //         'title': 'Google, Inc.',
+      //         'path': 'html>body>div>table>tbody>span',
+      //         'values': {
+      //           'valueHash01': {
+      //             'date': '2014-4-27',
+      //             'value': '50'
+      //           },
+      //           'valueHash02': {
+      //             'date': '2014-4-28',
+      //             'value': '54'
+      //           }
+      //         }
+      //       }
+      //     }
+      //   };
+
+      // console.log('Sending response to Firebase next...');
+      // db.set(response, function(err) {
+      //     if (err) {
+      //       console.log('Ther was an error saving to Firebase.  Error: ', err);
+      //     } else {
+      //       console.log('Data sent to Firebase successfully.');  
+      //     }
+      // });
+      
+      chrome.cookies.get({url: 'http://localhost:9000', name: 'firebaseSessionKey'},
+        function (cookie) {
+          if (cookie) {
+            console.log(cookie.value);
+            // CONTINUE FROM HERE: try getting the cookie stored in the background page
+            // Another option might be trying to store the cookie in the content script
+            chrome.cookies.set({url: 'chrome-extension://piokfjnabjpdlpgollhhcpdnbdnhcifp/_generated_background_page.html', name:'firebaseSessionKey'});
+          }
+          else {
+            console.log('Can\'t get cookie! Check the name!');
+          }
+        }
+      );
+
+
+      db.once('value', function(snapshot) {
+        console.log(snapshot.val());
       });
+
+
+
+
+
+      // chrome.tabs.sendMessage(tab.id, { createKiwi: true }, function(response) {
+      //   console.log('Right before sending to DB: ', response);
+      //   console.log('Sending to DB:');
+      //   db.push(response);
+      // });
     }
   );
 }
